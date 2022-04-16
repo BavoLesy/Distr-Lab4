e5f484f684e90f2bae9b682ec8b9cfcfcf4f09b2
Bavo Lesy <bavo.lesy@student.uantwerpen.be>
2022-04-16 13:33:02 +0200

    trying answer node

--- src/main/java/Node/DiscoveryNode.java
+++ src/main/java/Node/DiscoveryNode.java
@@ -121,6 +121,8 @@ public class DiscoveryNode extends Thread {
                 System.out.println("Discovery package received! -> " + receivePacket.getAddress() + ":" + receivePacket.getPort());
                 String s1 = "host0.group3.6dist" + receivePacket.getAddress().toString();
                 String s2 = InetAddress.getLocalHost().toString();
+                System.out.println(s1);
+                System.out.println(s2);
                 if(!s1.equals(s2)) {
                     String receivedData = new String(receivePacket.getData(), 0, receivePacket.getLength()).trim();
                     String IP = receivePacket.getAddress().getHostAddress(); //IP of the Current Node
@@ -137,7 +139,7 @@ public class DiscoveryNode extends Thread {
                         nextID = hash;
                         response = "{\"status\":\"OK\"," + "\"sender\":\"NodeNext\"," + "\"currentID\":" + currentID + "," +
                                 "\"nextID\":" + nextID + "\"}";
-                    } else if (hash < currentID && (previousID < hash || previousID == currentID)) { //
+                    } else if ((hash < currentID && previousID < hash) || (hash < currentID && previousID == currentID)) { //
                         previousID = hash;
                         response = "{\"status\":\"OK\"," + "\"sender\":\"NodePrevious\"," + "\"currentID\":" + currentID + "," +
                                 "\"previousID\":" + previousID + "\"}";
